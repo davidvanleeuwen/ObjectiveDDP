f8efd2c0701a5964a10adddcf7caaaf0a129b0c2
David van Leeuwen <david@madeawkward.com>
2015-05-16 19:09:29 +0200

    Adds support for iOS 8+ Framework and Carthage

---
+++ DDP/DDP.h
@@ -0,0 +1,26 @@
+//
+//  DDP.h
+//  DDP
+//
+//  Created by David van Leeuwen on 16/05/15.
+//  Copyright (c) 2015 Rebounds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DDP.
+FOUNDATION_EXPORT double DDPVersionNumber;
+
+//! Project version string for DDP.
+FOUNDATION_EXPORT const unsigned char DDPVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DDP/PublicHeader.h>
+
+#import <DDP/ObjectiveDDP.h>
+#import <DDP/MeteorClient.h>
+#import <DDP/MeteorClient+Private.h>
+#import <DDP/BSONIdGenerator.h>
+#import <DDP/DependencyProvider.h>
+
+
+
